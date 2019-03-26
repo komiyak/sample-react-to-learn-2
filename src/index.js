43b5315ddf5ca9237e0448b747c175e4607e2d0f
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-03-26 22:23:28 +0900

    Implementing a tick dom

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-};
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+    ReactDOM.render(element, document.getElementById('root'));
 }
 
-const element = <h1>Hello, {formatName(user)}</h1>;
-
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+setInterval(tick, 1000);
