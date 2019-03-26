0e1471cd5a0745ba5efe47f97f00bfa865c3522e
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-03-26 22:14:54 +0900

    Hello, world

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const element = <h1>Hello, {formatName(user)}</h1>;
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
 );
-
