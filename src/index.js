83521d85f7fb89ab6734f946d6b466a9a217c69a
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-03-28 22:10:59 +0900

    Update the time in a React's component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
+}
+
+// function component
 function tick() {
     const element = (
         <div>
@@ -12,4 +17,61 @@ function tick() {
     ReactDOM.render(element, document.getElementById('root'));
 }
 
-setInterval(tick, 1000);
+//setInterval(tick, 1000);
+
+// ReactDOM.render(
+//     <Welcome name="Sara"/>,
+//     document.getElementById('root')
+// );
+
+function FormattedDate(props) {
+    return <h2>It is {props.date.toLocaleTimeString()}</h2>
+}
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+
+    componentWillMount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <FormattedDate date={this.state.date}/>
+            </div>
+        );
+    }
+}
+
+function App() {
+    return (
+        <div>
+            <Clock/>
+            <Clock/>
+            <Clock/>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
